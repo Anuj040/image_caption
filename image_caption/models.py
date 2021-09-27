57de9efae809b2ff121c9e1f393a859d17002f67
Anuj040 <anujarora920804@gmail.com>
2021-09-27 18:30:28 +0900

    feat: encoder model for image embeddings

--- image_caption/models.py
+++ image_caption/models.py
@@ -32,13 +32,61 @@ class CNNModel(nn.Module):
 
         # Feature extractor layers
         features = self.model(inputs)
-        features = torch.reshape(features, (-1, features.shape[1]))
+        features = torch.reshape(features, (features.shape[0], -1, features.shape[1]))
         return features
 
 
+class TransformerEncoderBlock(nn.Module):
+
+    """encoder model for transforming image embeddings to one relatable to sequence embeddings"""
+
+    def __init__(
+        self,
+        output_embed_size: int,
+        num_heads: int,
+        input_embed_size: int = 1280,
+        **kwargs
+    ):
+        """initialize
+
+        Args:
+            output_embed_size (int): embedding size for the output sequences
+            num_heads (int):
+            input_embed_size (int, optional): embedding size for input sequences Defaults to 1280.
+        """
+        super().__init__(**kwargs)
+        self.output_embed_size = output_embed_size
+        self.num_heads = num_heads
+
+        self.attention_1 = nn.MultiheadAttention(
+            output_embed_size, num_heads, dropout=0.0, bias=True, batch_first=True
+        )
+        self.layernorm_1 = nn.LayerNorm(input_embed_size)
+        self.layernorm_2 = nn.LayerNorm(output_embed_size)
+        self.dense_1 = nn.Linear(input_embed_size, output_embed_size, bias=True)
+        self.act_1 = nn.ReLU()
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        """feature encoder's forward pass"""
+        inputs = self.layernorm_1(inputs)
+        inputs = self.act_1(self.dense_1(inputs))
+
+        attention_output_1, _ = self.attention_1(
+            query=inputs,
+            key=inputs,
+            value=inputs,
+            need_weights=True,
+            attn_mask=None,
+        )
+        out_1 = self.layernorm_2(inputs + attention_output_1)
+        return out_1
+
+
 if __name__ == "__main__":  # pragma: no cover
-    model = CNNModel()
+    cnn = CNNModel()
+    encoder = TransformerEncoderBlock(512, 1)
     a = torch.rand((3, 3, 299, 299))
 
-    b = model(a).detach().numpy()
-    print(b.shape)
+    b = cnn(a)
+    c = encoder(b)
+    print(c.detach().numpy().shape)
--- image_caption/train.py
+++ image_caption/train.py
@@ -1,7 +1,8 @@
 """model definition, train procedure and the essentials"""
 import torch
+from torch import nn
 
-from image_caption.models import CNNModel
+from image_caption.models import CNNModel, TransformerEncoderBlock
 
 # check if cuda available
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -10,8 +11,19 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 class Caption:
     """object for preparing model definition, train procedure and the essentials"""
 
-    def __init__(self, trainable: bool) -> None:
-        self.cnn_model = CNNModel(trainable=trainable).to(DEVICE)
+    def __init__(
+        self,
+        trainable: bool,
+    ) -> None:
+
+        output_embed_size: int = 512
+        num_heads: int = 1
+        input_embed_size: int = 1280
+
+        self.cnn_model: nn.Module = CNNModel(trainable=trainable).to(DEVICE)
+        self.encoder: nn.Module = TransformerEncoderBlock(
+            output_embed_size, num_heads=num_heads, input_embed_size=input_embed_size
+        ).to(DEVICE)
 
 
 if __name__ == "__main__":  # pragma: no cover
