badb8c01bb87133e5cfe933f04c3f7b1132a594e
Anuj040 <anujarora920804@gmail.com>
2021-11-07 18:56:58 +0900

    fix: embed scales

--- image_caption/models.py
+++ image_caption/models.py
@@ -40,7 +40,7 @@ class CNNModel(nn.Module):
         backbone = EfficientNet.from_pretrained("efficientnet-b0")
         backbone.requires_grad = trainable
         # Remove Unnecessary layers
-        backbone._bn1 = Identity()
+        # backbone._bn1 = Identity()  #
         backbone._avg_pooling = Identity()
         backbone._fc = Identity()
         backbone._swish = Identity()
@@ -61,6 +61,7 @@ class CNNModel(nn.Module):
         # Feature extractor layers
         features = self.model(inputs)
         features = torch.reshape(features, (features.shape[0], -1, self.img_embd_size))
+        # features = self.norm(features)
         return features
 
 
@@ -139,8 +140,7 @@ class PositionalEmbedding(nn.Module):
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         """forward pass for embedding generator"""
-        embedded_tokens = self.token_embeddings(inputs)
-        embedded_tokens = embedded_tokens * self.embed_scale
+        embedded_tokens = self.token_embeddings(inputs)  # * self.embed_scale
         if self.use_alibi:
             return embedded_tokens
 
