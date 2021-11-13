2e9cd20f2e54f3fc6730faf29ab063616f900ba8
Anuj040 <anujarora920804@gmail.com>
2021-11-13 12:10:55 +0900

    style: comments/docstrings alignment

--- image_caption/models_lstm.py
+++ image_caption/models_lstm.py
@@ -1,4 +1,6 @@
 """https://github.com/sgrvinod/a-PyTorch-Tutorial-to-Image-Captioning/blob/master/models.py"""
+from typing import Tuple
+
 import torch
 import torchvision
 from torch import nn
@@ -29,11 +31,15 @@ class Encoder(nn.Module):
 
         self.fine_tune()
 
-    def forward(self, images):
+    def forward(self, images: torch.Tensor) -> torch.Tensor:
         """
         Forward propagation.
-        :param images: images, a tensor of dimensions (batch_size, 3, image_size, image_size)
-        :return: encoded images
+
+        Args:
+            images (torch.Tensor): images, (batch_size, 3, image_size, image_size)
+
+        Returns:
+            torch.Tensor: encoded images
         """
         # (batch_size, 2048, image_size/32, image_size/32)
         out = self.resnet(images)
@@ -43,10 +49,12 @@ class Encoder(nn.Module):
         out = out.permute(0, 2, 3, 1)
         return out
 
-    def fine_tune(self, fine_tune=True):
+    def fine_tune(self, fine_tune: bool = True) -> None:
         """
         Tune the computation of gradients for convolutional blocks 2 through 4 of the encoder.
-        :param fine_tune: Allow?
+
+        Args:
+            fine_tune (bool, optional): Allow?. Defaults to True.
         """
         for param in self.resnet.parameters():
             param.requires_grad = False
@@ -68,34 +76,42 @@ class Attention(nn.Module):
         :param attention_dim: size of the attention network
         """
         super().__init__()
-        self.encoder_att = nn.Linear(
-            encoder_dim, attention_dim
-        )  # linear layer to transform encoded image
-        self.decoder_att = nn.Linear(
-            decoder_dim, attention_dim
-        )  # linear layer to transform decoder's output
-        self.full_att = nn.Linear(
-            attention_dim, 1
-        )  # linear layer to calculate values to be softmax-ed
+        # linear layer to transform encoded image
+        self.encoder_att = nn.Linear(encoder_dim, attention_dim)
+        # linear layer to transform decoder's output
+        self.decoder_att = nn.Linear(decoder_dim, attention_dim)
+        # linear layer to calculate values to be softmax-ed
+        self.full_att = nn.Linear(attention_dim, 1)
         self.relu = nn.ReLU()
-        self.softmax = nn.Softmax(dim=1)  # softmax layer to calculate weights
+        # softmax layer to calculate weights
+        self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, encoder_out, decoder_hidden):
+    def forward(
+        self, encoder_out: torch.Tensor, decoder_hidden: torch.Tensor
+    ) -> Tuple[torch.Tensor]:
+        """[summary]
+
+        Args:
+            encoder_out (torch.Tensor): encoded images, a tensor of dimension
+                                    (batch_size, num_pixels, encoder_dim)
+            decoder_hidden (torch.Tensor): previous decoder output
+                                    (batch_size, decoder_dim)
+
+        Returns:
+            torch.Tensor: attention weighted encoding
+            torch.Tensor: weights
         """
-        Forward propagation.
-        :param encoder_out: encoded images, a tensor of dimension (batch_size, num_pixels, encoder_dim)
-        :param decoder_hidden: previous decoder output, a tensor of dimension (batch_size, decoder_dim)
-        :return: attention weighted encoding, weights
-        """
-        att1 = self.encoder_att(encoder_out)  # (batch_size, num_pixels, attention_dim)
-        att2 = self.decoder_att(decoder_hidden)  # (batch_size, attention_dim)
-        att = self.full_att(self.relu(att1 + att2.unsqueeze(1))).squeeze(
-            2
-        )  # (batch_size, num_pixels)
-        alpha = self.softmax(att)  # (batch_size, num_pixels)
-        attention_weighted_encoding = (encoder_out * alpha.unsqueeze(2)).sum(
-            dim=1
-        )  # (batch_size, encoder_dim)
+
+        # (batch_size, num_pixels, attention_dim)
+        att1 = self.encoder_att(encoder_out)
+        # (batch_size, attention_dim)
+        att2 = self.decoder_att(decoder_hidden)
+        # (batch_size, num_pixels)
+        att = self.full_att(self.relu(att1 + att2.unsqueeze(1))).squeeze(2)
+        # (batch_size, num_pixels)
+        alpha = self.softmax(att)
+        # (batch_size, encoder_dim)
+        attention_weighted_encoding = (encoder_out * alpha.unsqueeze(2)).sum(dim=1)
 
         return attention_weighted_encoding, alpha
 
